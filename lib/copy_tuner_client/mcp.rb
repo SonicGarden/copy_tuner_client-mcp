d776e34d5f82fe18a37c86540da4e4fb1d9d057d
aki <lala.akira@gmail.com>
2025-06-18 11:44:09 +0900

    first commit

---
+++ lib/copy_tuner_client/mcp.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "mcp/version"
+
+module CopyTunerClient
+  module Mcp
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
